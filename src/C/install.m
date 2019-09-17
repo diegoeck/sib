ec9e4b698ac53606d4f5c0d07e654f7ead189e5b
Diego Eckhard <diegoeck@ufrgs.br>
2019-09-17 14:25:15 -0300

    Fix mex for windows

--- src/C/install.m
+++ src/C/install.m
@@ -1,4 +1,4 @@
 clc
-mex sib_oe_c.c sib_basic.c sib_optimize.c CFLAGS='\$CFLAGS -std=c99'  -I. -lmwlapack -lut -lmat
-mex sib_armax_c.c sib_basic.c sib_optimize.c CFLAGS='\$CFLAGS -std=c99'  -I./ -lmwlapack -lut -lmat
-mex sib_bj_c.c sib_basic.c sib_optimize.c CFLAGS='\$CFLAGS -std=c99'  -I./ -lmwlapack -lut -lmat
+mex sib_oe_c.c sib_basic.c sib_optimize.c -I. -lmwlapack -lut
+mex sib_armax_c.c sib_basic.c sib_optimize.c -I./ -lmwlapack -lut 
+mex sib_bj_c.c sib_basic.c sib_optimize.c -I./ -lmwlapack -lut 
